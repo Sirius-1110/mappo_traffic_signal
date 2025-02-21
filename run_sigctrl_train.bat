0196a9de890133d9fd5fbcc2e38e54d869108064
zou.dx <zou.dx@cidi.ai>
2025-02-21 10:45:36 +0800

    release v1: run model

--- run_sigctrl_train.bat
+++ run_sigctrl_train.bat
@@ -7,3 +7,11 @@ python sumo_train \
 python sumo_train \
 -e smartcross/envs/sumo_3roads_multi_agent_config.yaml \
 -d entry/sumo_config/sumo_3roads_mappo_baseline.py 
+
+python3 sumo_train \
+-e../smartcross/envs/sumo_3roads_multi_agent_config.yaml \
+-d entry/sumo_config/sumo_3roads_mappo_baseline.py
+
+python3 sumo_train \
+-e../smartcross/envs/sumo_7roads_multi_agent_config.yaml \
+-d entry/sumo_config/sumo_7roads_mappo_sota.py
